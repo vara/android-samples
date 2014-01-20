145c06821048a7fdf59f4b2f118863181f6bfc46
strycz <stryczniewicz@gmail.com>
2014-01-20 12:21:02 +0100

    3 rodzaje mapy (hybrid, normal, satellite)

--- applications/FirstDemo/src/com/example/firstdemo/MainActivity.java
+++ applications/FirstDemo/src/com/example/firstdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -16,10 +17,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity {
 
+    GoogleMap map;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
     }
 
 
@@ -27,6 +32,7 @@ public class MainActivity extends FragmentActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
@@ -36,6 +42,15 @@ public class MainActivity extends FragmentActivity {
         int i = item.getItemId();
         if (i == R.id.action_show_my_location) {
             goToMyLocation();
+        }
+        else if (i == R.id.menu_normal_map) {
+            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+        else if (i == R.id.menu_teren) {
+            map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+        else if (i == R.id.menu_hybrid) {
+            map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         }
 
         return super.onMenuItemSelected(featureId, item);
@@ -77,10 +92,12 @@ public class MainActivity extends FragmentActivity {
     private void goToMyLocation() {
 //        Log.i("MAIN", ": Go to my location");
 //
-        GoogleMap map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
         map.setMyLocationEnabled(true);
 
         LatLng myPosition = getMyPosition();
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPosition, 13));
 
         if (myPosition != null) {
 
